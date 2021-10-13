5ac62c16ec1336e64abb009b55be5118d57e39ec
wangjingyang.0424 <wangjingyang.0424@bytedance.com>
2021-10-13 18:34:13 +0800

    first commit

---
+++ src/test/java/com/bupt/GoodCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bupt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodCommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
