c9cae84c1bf39f42e72918d8cfbe098b317ee1f5
Jiacheng-Gao <2889886695@qq.com>
2021-11-04 21:11:00 +0800

    劳您驾我可以模块未测试

--- src/main/java/com/bupt/controller/LaoninjiaController.java
+++ src/main/java/com/bupt/controller/LaoninjiaController.java
@@ -1,12 +1,15 @@
 package com.bupt.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.Query;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.bupt.model.po.Laoninjia;
 import com.bupt.model.vo.Response;
 import com.bupt.service.LaoninjiaService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/laoninjia")
@@ -18,27 +21,64 @@ public class LaoninjiaController {
     @PostMapping("/query_all")//显示所有请求帮忙信息
     public Response query_all(){
 
-
+        List<Laoninjia> a=laoninjia.list();
+        return Response.ok(a);
 
     }
 
     @PostMapping("/query_user")//某用户请求信息
-    public Response query_user(){
+    public Response query_user(String id){
+
+        QueryWrapper<Laoninjia> tmp=new QueryWrapper<>();
+        tmp.eq("user_id",id);
+
+        return Response.ok(laoninjia.getOne(tmp));
 
     }
 
+
+    @PostMapping("/search_subject")    //主题名称模糊搜索
+    public Response search(String content)
+    {
+
+        QueryWrapper<Laoninjia> tmp=new QueryWrapper<>();
+        tmp.like("subject",content);
+        return Response.ok(laoninjia.list(tmp));
+    }
+
+
+    @PostMapping("/search_kind")    //帮忙类型搜索
+    public Response search2(String content)
+    {
+
+        QueryWrapper<Laoninjia> tmp=new QueryWrapper<>();
+        tmp.like("kind",content);
+        return Response.ok(laoninjia.list(tmp));
+    }
+
+
+
     @PostMapping("/publish")//发布新信息
-    public Response publish(){
+    public Response publish(@RequestBody Laoninjia entity){
+
+        laoninjia.saveOrUpdate(entity);
+        return Response.ok();
 
     }
 
     @PostMapping("/modify")//修改
-    public Response modify(){
+    public Response modify(@RequestBody Laoninjia entity){
+
+        laoninjia.saveOrUpdate(entity);
+        return Response.ok();
 
     }
 
     @PostMapping("/delete")//删除
-    public Response delete(){
+    public Response delete(@RequestBody Laoninjia entity){
+
+        laoninjia.removeById(entity);
+        return Response.ok();
 
     }
 
--- src/main/java/com/bupt/controller/WokeyiController.java
+++ src/main/java/com/bupt/controller/WokeyiController.java
@@ -1,12 +1,11 @@
 package com.bupt.controller;
 
 
+import com.bupt.model.po.Wokeyi;
 import com.bupt.model.vo.Response;
 import com.bupt.service.WokeyiService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/wokeyi")
@@ -15,23 +14,28 @@ public class WokeyiController {
     @Autowired
     WokeyiService wokeyiService;
 
-    @PostMapping("/query")//显示同社区的劳您驾信息
-    public Response query(){
-
-    }
 
     @PostMapping("/publish")//提交新的响应信息
-    public Response publish(){
+    public Response publish(@RequestBody Wokeyi entity){
+
+        wokeyiService.saveOrUpdate(entity);
+        return Response.ok();
 
     }
 
     @PostMapping("/modify")//修改自己的已提交未接受响应信息
-    public Response modify(){
+    public Response modify(@RequestBody Wokeyi entity){
+
+        wokeyiService.saveOrUpdate(entity);
+        return Response.ok();
 
     }
 
     @PostMapping("/delete")//删除已提交未接受响应信息
-    public Response delete(){
+    public Response delete(@RequestBody Wokeyi entity){
+
+        wokeyiService.removeById(entity);
+        return Response.ok();
 
     }
 
